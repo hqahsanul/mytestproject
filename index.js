0bf22b55dbef4d9d4e22bfe0e59994a765a3a652
hqahsanul <hqahsanul@gmail.com>
2021-11-11 11:22:24 +0530

    added mongoose and connected to the mongodb

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.static("public"));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  name: String,
+  query: String,
+});
 
 app.listen(3000, function () {
   console.log("Server has started at port 3000");
