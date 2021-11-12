e44cdd73f9fb1c601c1827926478da9d1e659771
hqahsanul <hqahsanul@gmail.com>
2021-11-12 10:54:28 +0530

    added thank you page

--- index.js
+++ index.js
@@ -9,16 +9,48 @@ app.use(
   })
 );
 
-mongoose.connect("mongodb://localhost:27017/userDB", {
+mongoose.connect("mongodb://localhost:27017/contactDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-mongoose.set("useCreateIndex", true);
 
-const userSchema = new mongoose.Schema({
+const usersSchema = new mongoose.Schema({
   email: String,
   name: String,
   query: String,
+});
+
+const User = mongoose.model("User", usersSchema);
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/form.html");
+});
+
+app.post("/", function (req, res) {
+  const emailName = req.body.email;
+  const userName = req.body.name;
+  const queryName = req.body.query;
+  console.log(emailName);
+
+  const user = new User({
+    email: emailName,
+    name: userName,
+    query: queryName,
+  });
+
+  user.save();
+  console.log("Done");
+  res.sendFile(__dirname + "/done.html");
+
+  User.find(function (err, users) {
+    if (err) {
+      console.log(err);
+    } else {
+      users.forEach(function (user) {
+        console.log(user.name);
+      });
+    }
+  });
 });
 
 app.listen(3000, function () {
